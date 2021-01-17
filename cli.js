e7b379ee3022cd2572f8bf9d41594cdc9f70c16c
EC2 Default User <ec2-user@ip-172-31-14-120.us-east-2.compute.internal>
2021-01-17 22:55:01 +0000

    polished the UI deleted out the console.debug(), also delted the console.log() method calls that were unnecasary. method calls

--- cli.js
+++ cli.js
@@ -9,6 +9,8 @@ const filePath = relative('/', process.argv[2]);
 //Get command
 const usersCommand = process.argv.slice(3);
 
+//Message 
+console.log(`    Listening To: ${ process.argv[2]}\nListener Command: ${usersCommand.join(' ')}`);
 
 // set the listener
 onSave(filePath, usersCommand);
