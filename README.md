999437ea7f102f90e3b28f8f55dfaa6437d68a44
EC2 Default User <ec2-user@ip-172-31-14-120.us-east-2.compute.internal>
2021-01-17 18:33:11 +0000

    I uploaded my code that I wrote during experimental testing, and organized to prepare it to be shipped to npm

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1> onsave </h1>
+Welcome! thanks for stoping by.
+`onsave` aims to be the simplest and most beginer friendly automation tool for node.js!
+
+documentation coming soon
+
+
+
---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+/*
+const { spawn } = require('child_process');
+
+
+spawn('echo', ['hello world'], {
+  stdio: ['inherit', 'inherit', 'inherit']
+})
+*/
+
+console.log('hello');
