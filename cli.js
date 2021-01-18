87953ae9e2b5aa4cf568ed590afde823127d7938
EC2 Default User <ec2-user@ip-172-31-14-120.us-east-2.compute.internal>
2021-01-18 11:47:11 +0000

    fixed bug for path.relative() on darwin operating systems added process.platform logic

--- cli.js
+++ cli.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 'use strict';
-const { relative } = require('path');
+const { relative, isAbsolute, join } = require('path');
 const onSave = require('./lib/onSave2.js');
 
 //Get File Path
-const filePath = relative('/', process.argv[2]);
-
+let filePath = relative('/', process.argv[2]);
+if (!isAbsolute(filePath)) {
+  filePath = join('/', filePath);
+}
 //Get command
 const usersCommand = process.argv.slice(3);
+
 
 //Message 
 console.log(`    Listening To: ${ process.argv[2]}\nListener Command: ${usersCommand.join(' ')}`);
