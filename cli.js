25ad8c445bb9bef5abe43fd5ab2cfbd61983111a
EC2 Default User <ec2-user@ip-172-31-14-120.us-east-2.compute.internal>
2021-01-18 12:56:53 +0000

    changed path.relative to path.resolve for getting the absolute path to the file being watched

--- cli.js
+++ cli.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 'use strict';
-const { relative, isAbsolute, join } = require('path');
+const { resolve } = require('path');
 const onSave = require('./lib/onSave2.js');
 
 //Get File Path
-let filePath = relative('/', process.argv[2]);
-if (!isAbsolute(filePath)) {
-  filePath = join('/', filePath);
-}
+let filePath = resolve(process.argv[2]);
+console.debug(filePath);
 //Get command
 const usersCommand = process.argv.slice(3);
 
